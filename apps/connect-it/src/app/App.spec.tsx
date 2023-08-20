cac853ef50c90969d4f280f7d90faa34e5ee9bee
Ankit Lakra <ankit.lakra@datapeace.in>
2023-08-20 20:46:32 +0530

    fix(connect-it): fixed test

--- apps/connect-it/src/app/App.spec.tsx
+++ apps/connect-it/src/app/App.spec.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
+import { expect } from 'detox';
 
 import App from './App';
 
 test('renders correctly', () => {
   const { getByTestId } = render(<App />);
-  expect(getByTestId('heading')).toHaveTextContent('Welcome');
+  expect(getByTestId('heading')).toHaveText('Welcome');
 });
