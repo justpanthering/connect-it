3268f87dcccfd29636b6226a33ee497ffe2d5e6b
Ankit Lakra <ankit.lakra@datapeace.in>
2023-08-20 20:01:07 +0530

    feat(connect-it): added tamagui and related configuration

--- apps/connect-it/src/app/App.tsx
+++ apps/connect-it/src/app/App.tsx
@@ -5,17 +5,21 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { ROUTES } from './utils/navigation';
 import { Home } from './screens';
+import { TamaguiProvider } from 'tamagui';
+import config from '../../tamagui.config';
 
 export const App = () => {
   const Stack = createStackNavigator();
 
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="dark-content" />
-      <Stack.Navigator>
-        <Stack.Screen name={ROUTES.HOME} component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <TamaguiProvider config={config}>
+      <NavigationContainer>
+        <StatusBar barStyle="dark-content" />
+        <Stack.Navigator>
+          <Stack.Screen name={ROUTES.HOME} component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </TamaguiProvider>
   );
 };
 
--- apps/connect-it/src/app/screens/Home.tsx
+++ apps/connect-it/src/app/screens/Home.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'tamagui';
 
 export function Home() {
   return (
     <View>
       <Text>Home</Text>
+      <Button>
+        <Text>Button</Text>
+      </Button>
     </View>
   );
 }
